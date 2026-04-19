7456ff9b87c1b50ac841c2c52350f177d36948e3
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-04-19 12:41:50 -0400

    Delete moreTest.java

--- moreTest.java
+++
@@ -1,9 +0,0 @@
-public class moreTest {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Iteration: " + i);
-        }
-
-        System.out.println(5);
-    }
-}

db59dca17c577e08e29c68a8210e203bad2dfcce
Savya Vats <savyavats@gmail.com>
2026-04-25 19:08:40 -0400

    created basic functions

--- CSA-Recursion-String-Decoder/Methods.java
+++ CSA-Recursion-String-Decoder/Methods.java
@@ -1,3 +1,12 @@
 public class Methods {
-    
+    public Methods() {
+    }
+
+    public String convert(String nums) {
+        return "";
+    }
+
+    public String decode(String encString) {
+        return "";
+    }
 }

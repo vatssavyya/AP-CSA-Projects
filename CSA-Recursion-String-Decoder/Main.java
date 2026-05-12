0204fd126197b95ae192d33198a5a54dcf8f7ee0
Savya Vats <savyavats@gmail.com>
2026-05-12 11:26:35 -0400

    working on decoder

--- CSA-Recursion-String-Decoder/Main.java
+++ CSA-Recursion-String-Decoder/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // this should be an input for the ciphering
         String var1 = Methods.convert("4 43 103 168 51 227 219 231 128 247 187 239 168 115 179 203 187 223 179 187");
-        System.out.println(var1);
+        System.out.println(Methods.decode(var1));
+
     }
 }
 
--- CSA-Recursion-String-Decoder/Methods.java
+++ CSA-Recursion-String-Decoder/Methods.java
@@ -10,9 +10,28 @@ public class Methods {
         }
         return jumbled;
     }
-    //do 2 * mod 257 cuz it guarentees a return to 1
+    //do 2 * mod 257 cuz it guarantees a return to 1
     public static String decode(String encString) {
+        String unscrambled = "";
+        char[] arrayTemp = new char[encString.length()];
+        for (int i = 0; i < encString.length(); i++) {
+            arrayTemp[i] = encString.charAt(i);
+        }
+        while (arrayTemp[0] != 1) {
+            for (int j = 0; j < encString.length(); j++) {
+                arrayTemp[j] = (char)((arrayTemp[j] * 2) % 257);
+            }
+        }
+        for (int k = 0; k < arrayTemp.length; k++) {
+            if ((arrayTemp[k] - 10) < 32) {
+                
+                arrayTemp[k] = (char)(32);
+            }
+            arrayTemp[k] = (char)(arrayTemp[k]);
+        }
 
-        return "";
+
+
+        return unscrambled;
     }
 }

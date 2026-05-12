46105556562b87516494a7fd00e9ca658e57979b
Savya Vats <savyavats@gmail.com>
2026-05-11 21:52:31 -0400

    convert done

--- CSA-Recursion-String-Decoder/Methods.java
+++ CSA-Recursion-String-Decoder/Methods.java
@@ -3,9 +3,14 @@ public class Methods {
     }
     //should be unreadable, this ciphers into it
     public String convert(String nums) {
-        return "";
+        String jumbled = "";
+        String[] arrayThing = nums.split(" ");
+        for (int i = 0; i < arrayThing.length; i++) {
+            jumbled += (char)(Integer.parseInt(arrayThing[i]));
+        }
+        return jumbled;
     }
-
+    //do 2 * mod 257 cuz it guarentees a return to 1
     public String decode(String encString) {
         return "";
     }

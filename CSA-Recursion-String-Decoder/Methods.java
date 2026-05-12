2c7b66705b2751f03c4a4dc24e1e44f9a0da9193
Savya Vats <savyavats@gmail.com>
2026-05-12 08:46:12 -0400

    testing

--- CSA-Recursion-String-Decoder/Methods.java
+++ CSA-Recursion-String-Decoder/Methods.java
@@ -2,7 +2,7 @@ public class Methods {
     public Methods() {
     }
     //should be unreadable, this ciphers into it
-    public String convert(String nums) {
+    public static String convert(String nums) {
         String jumbled = "";
         String[] arrayThing = nums.split(" ");
         for (int i = 0; i < arrayThing.length; i++) {
@@ -11,7 +11,8 @@ public class Methods {
         return jumbled;
     }
     //do 2 * mod 257 cuz it guarentees a return to 1
-    public String decode(String encString) {
+    public static String decode(String encString) {
+
         return "";
     }
 }
